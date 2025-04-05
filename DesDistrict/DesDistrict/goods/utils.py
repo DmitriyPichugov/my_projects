644a0127e43a1d635ecbbe70617e3036888f3b48
DmitriyP <dapichugov@edu.hse.ru>
2025-04-05 09:12:47 +0300

    PostgreSQL database connected & full text search well done

--- DesDistrict/DesDistrict/goods/utils.py
+++ DesDistrict/DesDistrict/goods/utils.py
@@ -1,4 +1,10 @@
 from django.db.models import Q
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 from goods.models import Products
 
@@ -7,12 +13,30 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description", config="russian")
+    query = SearchQuery(query, config="russian")
+
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline_name=SearchHeadline(
+            "name",
+            query,
+            config="russian",
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        ),
+        headline_description=SearchHeadline(
+            "description",
+            query,
+            config="russian",
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        ),
+    )
     
-    q_objects = Q()
-    
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-    
-    return Products.objects.filter(q_objects)
+    return result
