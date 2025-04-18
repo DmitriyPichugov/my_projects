fad864c2c91c541ba433e510d85e46c7c21d9876
DmitriyP <dapichugov@edu.hse.ru>
2025-04-18 14:37:44 +0300

    Create order is ready

--- DesDistrict/DesDistrict/users/admin.py
+++ DesDistrict/DesDistrict/users/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
 
+from orders.admin import OrderTabulareAdmin
 from carts.admin import CartTabAdmin
 from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = [
+    list_display = (
         "username",
         "first_name",
         "last_name",
-        "email"
-    ]
-    search_fields = [
+        "email",
+    )
+    search_fields = (
         "username",
         "first_name",
         "last_name",
-        "email"
-    ]
-    inlines = [CartTabAdmin]
+        "email",
+    )
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
