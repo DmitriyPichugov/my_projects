90159742b7434a78f8b3e2770800023ff0e76d00
DmitriyP <dapichugov@edu.hse.ru>
2025-04-18 16:37:55 +0300

    Fixed all bugs

--- DesDistrict/DesDistrict/goods/views.py
+++ DesDistrict/DesDistrict/goods/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_list_or_404, render
+from django.http import Http404
+from django.shortcuts import render
 
 from goods.utils import q_search
 from goods.models import Products
@@ -17,8 +18,9 @@ def catalog(request, category_slug=None):
     elif query:
         goods = q_search(query)
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-
+        goods = Products.objects.filter(category__slug=category_slug)
+        if not goods.exists():
+            raise Http404()
     if on_sale:
         goods = goods.filter(discount__gt=0)
     
@@ -39,8 +41,6 @@ def catalog(request, category_slug=None):
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
     
-    context = {
-        'product': product
-    }
+    context = {'product': product}
     
-    return render(request, 'goods/product.html', context=context)
+    return render(request, 'goods/product.html', context)
