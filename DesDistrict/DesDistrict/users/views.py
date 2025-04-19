aac7d16d8aa054e3357d9cc47fccd43aa8b7b8d3
DmitriyP <dapichugov@edu.hse.ru>
2025-04-19 13:30:03 +0300

    Added low cache API & formate views

--- DesDistrict/DesDistrict/users/views.py
+++ DesDistrict/DesDistrict/users/views.py
@@ -1,4 +1,4 @@
-from urllib import request
+from django.core.cache import cache
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,26 +9,26 @@ from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, TemplateView, UpdateView
 
+from common.mixins import CacheMixin
 from orders.models import Order, OrderItem
 from carts.models import Cart
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 class UserLoginView(LoginView):
-    template_name = 'users/login.html'
+    template_name = "users/login.html"
     form_class = UserLoginForm
-    #success_url = reverse_lazy('main:index')
-    
+
     def get_success_url(self):
         redirect_page = self.request.POST.get("next", None)
         if redirect_page and redirect_page != reverse("user:logout"):
             return redirect_page
-        return reverse_lazy('main:index')
-    
+        return reverse_lazy("main:index")
+
     def form_valid(self, form):
         session_key = self.request.session.session_key
         user = form.get_user()
-        
+
         if user:
             auth.login(self.request, user)
             if session_key:
@@ -36,74 +36,86 @@ class UserLoginView(LoginView):
                 if forgot_carts.exists():
                     forgot_carts.delete()
                 Cart.objects.filter(session_key=session_key).update(user=user)
-                
+
                 messages.success(self.request, f"{user.username}, Вы вошли в аккаунт")
                 return HttpResponseRedirect(self.get_success_url())
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'DesDistrict - Авторизация'
+        context["title"] = "DesDistrict - Авторизация"
         return context
 
 
 class UserRegistrationView(CreateView):
-    template_name = 'users/registration.html'
+    template_name = "users/registration.html"
     form_class = UserRegistrationForm
-    success_url = reverse_lazy('users:profile')
-    
+    success_url = reverse_lazy("users:profile")
+
     def form_valid(self, form):
         session_key = self.request.session.session_key
         user = form.instance
-        
+
         if user:
             form.save()
             auth.login(self.request, user)
         if session_key:
             Cart.objects.filter(session_key=session_key).update(user=user)
-        
-        messages.success(self.request, f"{user.username}, Вы успешно зарегистрировались и вошли в аккаунт")
+
+        messages.success(
+            self.request,
+            f"{user.username}, Вы успешно зарегистрировались и вошли в аккаунт",
+        )
         return HttpResponseRedirect(self.success_url)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'DesDistrict - Регистрация'
+        context["title"] = "DesDistrict - Регистрация"
         return context
 
 
-class UserProfileView(LoginRequiredMixin, UpdateView):
-    template_name = 'users/profile.html'
+class UserProfileView(LoginRequiredMixin, CacheMixin, UpdateView):
+    template_name = "users/profile.html"
     form_class = ProfileForm
-    success_url = reverse_lazy('users:profile')
-    
-    def get_object(self, queryset = ...):
+    success_url = reverse_lazy("users:profile")
+
+    def get_object(self, queryset=...):
         return self.request.user
 
     def form_valid(self, form):
         messages.success(self.request, "Профиль успешно обновлен")
         return super().form_valid(form)
-    
+
     def form_invalid(self, form):
         messages.error(self.request, "Произошла ошибка")
         return super().form_invalid(form)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'DesDistrict - Кабинет'
-        context['orders'] = Order.objects.filter(user=self.request.user).prefetch_related(
-            Prefetch(
-                "orderitem_set",
-                queryset=OrderItem.objects.select_related("product"),
+        context["title"] = "DesDistrict - Кабинет"
+
+        orders = (
+            Order.objects.filter(user=self.request.user)
+            .prefetch_related(
+                Prefetch(
+                    "orderitem_set",
+                    queryset=OrderItem.objects.select_related("product"),
+                )
             )
-        ).order_by("-id")
+            .order_by("-id")
+        )
+
+        context["orders"] = self.set_get_cache(
+            orders, f"user_{self.request.user.id}_orders", 60 * 2
+        )
         return context
 
 
 class UserCartView(TemplateView):
-    template_name = 'users/users_cart.html'
+    template_name = "users/users_cart.html"
 
     def get_context_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
-        context['title'] = 'DesDistrict - Корзина'
+        context = super().get_context_data(**kwargs)
+        context["title"] = "DesDistrict - Корзина"
         return context
 
 
