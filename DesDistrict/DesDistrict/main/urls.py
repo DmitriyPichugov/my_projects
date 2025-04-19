449d755ebe933b87f045cf275ab395a9a2d11c23
DmitriyP <dapichugov@edu.hse.ru>
2025-04-19 13:30:03 +0300

    Added low cache API & formate views

--- DesDistrict/DesDistrict/main/urls.py
+++ DesDistrict/DesDistrict/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from main import views
 
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('about/', views.AboutView.as_view(), name='about')
+    path("", views.IndexView.as_view(), name="index"),
+    path("about/", views.AboutView.as_view(), name="about"),
 ]
--- DesDistrict/DesDistrict/main/views.py
+++ DesDistrict/DesDistrict/main/views.py
@@ -6,38 +6,23 @@ from goods.models import Categories
 
 
 class IndexView(TemplateView):
-    template_name = 'main/index.html'
+    template_name = "main/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'DesDistrict - Главная'
-        context['content'] = 'Магазин мебели DesDistrict'
+        context["title"] = "DesDistrict - Главная"
+        context["content"] = "Магазин мебели DesDistrict"
         return context
-        
+
 
 class AboutView(TemplateView):
-    template_name = 'main/about.html'
-    
+    template_name = "main/about.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'DesDistrict - О нас'
-        context['content'] = 'О нас'
-        context['text_on_page'] = 'Текст о том, почему этот магазин такой классныйи какой хороший товар'
+        context["title"] = "DesDistrict - О нас"
+        context["content"] = "О нас"
+        context["text_on_page"] = (
+            "Текст о том, почему этот магазин такой классныйи какой хороший товар"
+        )
         return context
-    
-# def index(request):
-#     context = {
-#         'title': 'DesDistrict - Главная',
-#         'content': 'Магазин мебели DesDistrict'
-#     }
-    
-#     return render(request, 'main/index.html', context)
-
-# def about(request):
-#     context = {
-#         'title': 'DesDistrict - О нас',
-#         'content': 'О нас',
-#         'text_on_page': 'Текст о том, почему этот магазин такой классныйи какой хороший товар'
-#     }
-    
-#     return render(request, 'main/about.html', context)
